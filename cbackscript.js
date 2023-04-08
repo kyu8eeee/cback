226fa90788f43de3337103252cbe20fe8a4ff2a2
kyu8eeee <130030554+kyu8eeee@users.noreply.github.com>
2023-04-08 23:37:09 +0900

    Update cbackscript.js

--- cbackscript.js
+++ cbackscript.js
@@ -5,7 +5,7 @@ setTimeout(function() {
   if (popupClosed) {
     popup.style.display = 'block';
   }
-}, 3000); 
+}, 5000); 
 
 link.addEventListener('click', function() {
   popupClosed = false;
@@ -13,11 +13,11 @@ link.addEventListener('click', function() {
     if (!window.closed) {
       popup.style.display = 'none';
     }
-  }, 3000);
+  }, 5000);
 });
 
 window.addEventListener('popstate', function(event) {
-  if (event.state && event.state.isLinkPage && event.state.timeSpent >= 3000) {
+  if (event.state && event.state.isLinkPage && event.state.timeSpent >= 5000) {
     popupClosed = false;
     popup.style.display = 'none';
   }
